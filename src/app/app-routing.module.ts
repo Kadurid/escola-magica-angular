5c76d33190de1ef320374262aa52bd2a1da90005
Kadurid <kaduridu@gmail.com>
2021-10-16 14:42:50 -0300

    feat(configuracao rotas): configuracao da navbar e das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { DisciplinaComponent } from './components/disciplina/disciplina.component';
+import { ProfessorComponent } from './components/professor/professor.component';
+import { AlunoComponent } from './components/aluno/aluno.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'alunos', component: AlunoComponent},
+  {path:'professores', component: ProfessorComponent},
+  {path:'disciplinas', component: DisciplinaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
