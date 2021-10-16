8ee6267b6d026345ab6fd231ece4ee241fe2b83e
Eloy <eeloy10@gmail.com>
2021-10-16 16:42:37 -0300

    adicao professor manual, inicio do form disciplina

--- src/app/components/disciplina/disciplina.component.ts
+++ src/app/components/disciplina/disciplina.component.ts
@@ -2,28 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BaseFormComponent } from 'src/app/shared/components/base-form/base-form.component';
 import { Disciplina } from 'src/app/shared/core/model/disciplina.model';
+import { IProfessor } from 'src/app/shared/core/model/professor.model';
 import { DisciplinaService } from 'src/app/shared/core/service/disciplina.service';
+import { ProfessorService } from 'src/app/shared/core/service/professor.service';
 
 @Component({
   selector: 'app-disciplina',
   templateUrl: './disciplina.component.html'
 })
 export class DisciplinaComponent extends BaseFormComponent implements OnInit {
-
   formulario!: FormGroup;
+
+  professores: IProfessor[] | undefined;
 
   constructor(
     private fb: FormBuilder,
-    private service: DisciplinaService
+    private service: DisciplinaService,
+    private professorService: ProfessorService
   ) {
     super();
   }
 
   ngOnInit(): void {
-    this.criarForm();
+    this.createForm();
+    this.getProfessor();
   }
 
-  criarForm() {
+  createForm() {
     this.fb.group({
       id: [null],
       nome: [null],
@@ -32,6 +37,10 @@ export class DisciplinaComponent extends BaseFormComponent implements OnInit {
       revogada: [null],
       cargaHoraria: [null]
     })
+  }
+
+  getProfessor() {
+    this.professores = this.professorService.findAll();
   }
 
   submit(){
--- src/app/shared/core/service/professor.service.ts
+++ src/app/shared/core/service/professor.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from "@angular/core";
+import { ClasseENUM } from "../enum/classe.enum";
+import { SubClasseENUM } from "../enum/sub-classe.enum";
 import { Professor } from "../model/professor.model";
 
 @Injectable({ providedIn: 'root' })
@@ -12,7 +14,11 @@ export class ProfessorService {
     }
 
     findAll(): Professor[] {
-        console.log('carregando professores');
-        return new Array();
+        let ps = new Array();
+        ps.push(new Professor(1, 'magin', ClasseENUM.CLERIGO, SubClasseENUM.TECNOMAGO, 123, 'muito bom'));
+        ps.push(new Professor(2, 'bardin', ClasseENUM.BARDO, SubClasseENUM.GUERREIROARCANO, 1239, 'muito bom'));
+        ps.push(new Professor(3, 'obscuro', ClasseENUM.ARCANISTA, SubClasseENUM.TECNOMAGO, 153, 'muito bom'));
+        ps.push(new Professor(4, 'elfin', ClasseENUM.GUERREIRO, SubClasseENUM.BARDOENTUSIASTA, 113, 'muito bom'));
+        return ps;
     }
 }
