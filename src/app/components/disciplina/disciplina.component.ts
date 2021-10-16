397b5e46eeac133ef0c25895de69763d5fa11a21
Eloy <eeloy10@gmail.com>
2021-10-16 16:07:38 -0300

    card form

--- src/app/components/disciplina/disciplina.component.ts
+++ src/app/components/disciplina/disciplina.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { BaseFormComponent } from 'src/app/shared/components/base-form/base-form.component';
+import { Disciplina } from 'src/app/shared/core/model/disciplina.model';
+import { DisciplinaService } from 'src/app/shared/core/service/disciplina.service';
 
 @Component({
   selector: 'app-disciplina',
-  templateUrl: './disciplina.component.html',
-  styleUrls: ['./disciplina.component.scss']
+  templateUrl: './disciplina.component.html'
 })
-export class DisciplinaComponent implements OnInit {
+export class DisciplinaComponent extends BaseFormComponent implements OnInit {
 
-  constructor() { }
+  formulario!: FormGroup;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private service: DisciplinaService
+  ) {
+    super();
   }
 
+  ngOnInit(): void {
+    this.criarForm();
+  }
+
+  criarForm() {
+    this.fb.group({
+      id: [null],
+      nome: [null],
+      professorResponsavel: [null],
+      alunos: [null],
+      revogada: [null],
+      cargaHoraria: [null]
+    })
+  }
+
+  submit(){
+    console.log(this.formulario);
+    let disciplina = new Disciplina();
+    this.service.create(disciplina);
+
+  }
 }
