deefbdcc3bec2ec16dc3d678c22824157f24f603
Eloy <eeloy10@gmail.com>
2021-10-16 17:08:45 -0300

    selects disciplina

--- src/app/components/disciplina/disciplina.component.ts
+++ src/app/components/disciplina/disciplina.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { BaseFormComponent } from 'src/app/shared/components/base-form/base-form.component';
+import { IAluno } from 'src/app/shared/core/model/aluno.model';
 import { Disciplina } from 'src/app/shared/core/model/disciplina.model';
 import { IProfessor } from 'src/app/shared/core/model/professor.model';
+import { AlunoService } from 'src/app/shared/core/service/aluno.service';
 import { DisciplinaService } from 'src/app/shared/core/service/disciplina.service';
 import { ProfessorService } from 'src/app/shared/core/service/professor.service';
 
@@ -14,11 +16,14 @@ export class DisciplinaComponent extends BaseFormComponent implements OnInit {
   formulario!: FormGroup;
 
   professores: IProfessor[] | undefined;
+  alunosDisponiveis: IAluno[] | undefined;
+  alunosAdicionados: IAluno[] | undefined;
 
   constructor(
     private fb: FormBuilder,
     private service: DisciplinaService,
-    private professorService: ProfessorService
+    private professorService: ProfessorService,
+    private alunoService: AlunoService
   ) {
     super();
   }
@@ -26,6 +31,7 @@ export class DisciplinaComponent extends BaseFormComponent implements OnInit {
   ngOnInit(): void {
     this.createForm();
     this.getProfessor();
+    this.getAluno();
   }
 
   createForm() {
@@ -41,6 +47,10 @@ export class DisciplinaComponent extends BaseFormComponent implements OnInit {
 
   getProfessor() {
     this.professores = this.professorService.findAll();
+  }
+
+  getAluno() {
+    this.alunosDisponiveis = this.alunoService.findAll();
   }
 
   submit(){
--- src/app/shared/core/service/aluno.service.ts
+++ src/app/shared/core/service/aluno.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { EscolaridadeENUM } from "../enum/escolaridade.enum";
 import { Aluno } from "../model/aluno.model";
 
 @Injectable({ providedIn: 'root' })
@@ -11,7 +12,18 @@ export class AlunoService {
     }
 
     findAll(): Aluno[] {
-        console.log('carregando alunos');
-        return new Array();
+        let ps = new Array();
+        ps.push(new Aluno('202101et', 'Jao', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202102et', 'Maria', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202103et', 'Enzo', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202104et', 'Valentina', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202105et', 'Geruza', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202106et', 'Tam', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202107et', 'Igor', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202108et', 'Breno', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202109et', 'Italo', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202110et', 'Jaozinho', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        ps.push(new Aluno('202111et', 'Mariazinha', 'CLERIGO', '10/10/199', 12, EscolaridadeENUM.INICIANTE, [], '', '', [], [], 'uma ai'));
+        return ps;
     }
 }
